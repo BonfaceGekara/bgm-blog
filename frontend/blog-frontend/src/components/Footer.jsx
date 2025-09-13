0a9f4b7ba65ba3413a9dd06ad54d44b323f12d97
BonfaceGekara <bonface248gekara@gmail.com>
2025-09-14 00:14:27 +0300

    styled footer

--- frontend/blog-frontend/src/components/Footer.jsx
+++ frontend/blog-frontend/src/components/Footer.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Footer() {
-
+    return(
+        <footer class="bg-secondary text-white text-center py-3 me-auto">
+            A blog by B8Fire &copy; 2025
+        </footer>
+    );
 }
 
 export default Footer;
--- frontend/blog-frontend/src/components/Header.jsx
+++ frontend/blog-frontend/src/components/Header.jsx
@@ -3,23 +3,23 @@ import React from "react";
 function Header() {
     return (
         <header>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom shadow-sm">
+            <nav class="navbar navbar-expand-lg navbar-light bg-secondary border-bottom">
                 <div class="container">
                     
-                    <a class="navbar-brand fw-bold" href="#">BGM-Blog</a>
+                    <a class="navbar-brand text-white fw-bold" href="#">BGM-Blog</a>
                     
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent"
                         aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     
-                    <div class="collapse navbar-collapse justify-content-end" id="navbarContent">
+                    <div class="collapse navbar-collapse navbar-dark justify-content-end" id="navbarContent">
                         <ul class="navbar-nav">
                             <li class="nav-item">
-                                <a class="nav-link" href="/login">Login</a>
+                                <a class="nav-link" href="#">Login</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/register">Register</a>
+                                <a class="nav-link" href="#">Register</a>
                             </li>
                         </ul>
                     </div>
